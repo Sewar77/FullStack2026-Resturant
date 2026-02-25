ea1dbfedf4551b26fdb2c7558146e5759e136c28
Sewar77 <sewarjihad793@gmail.com>
2026-02-25 11:37:52 +0300

    feat: enhance user registration process with improved validation and error handling
    fix role array problem

--- server/src/controllers/auth.Controller.js
+++ server/src/controllers/auth.Controller.js
@@ -5,19 +5,26 @@ import { findUserByEmail } from "../models/user.Model.js";
 
 export const register = async (req, res) => {
     const { name, email, password } = req.validateData
+    console.log(req.validateData)
     try {
         const existedUser = await findUserByEmail(email)
         if (existedUser) {
             return res.status(400).json({ message: "email already exists" })
         }
-
+        console.log(12)
         const hashed_password = await bcrypt.hash(password, 10)
+        console.log(13)
+        console.log(password)
 
-        const newUser = await createUser(name, email, hashed_password, "user")
+        console.log(hashed_password)
+
+        const newUser = await createUser(name, email, hashed_password, "{user}")
+        console.log(newUser)
 
         if (!newUser) {
             return res.status(400).json({ message: "failed to create user" })
         }
+        console.log(16)
 
         return res.status(201).json({ message: "user created successfully", user: newUser })
     }
