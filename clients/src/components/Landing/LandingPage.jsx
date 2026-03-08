f90b0f896680eef462e109f220962f6cb52522f1
Sewar77 <sewarjihad793@gmail.com>
2026-03-08 14:57:15 +0300

    create user context
    create register form
    
    create app provider

--- clients/src/components/Landing/LandingPage.jsx
+++ clients/src/components/Landing/LandingPage.jsx
@@ -1,9 +1,11 @@
 import Header from "../Shared/Header.jsx";
 import Footer from "../Shared/Footer.jsx";
+import Register from "../Auth/Register.jsx";
 function LandingPage() {
   return (
     <>
       <Header />
+      <Register />
       <Footer />
     </>
   );
